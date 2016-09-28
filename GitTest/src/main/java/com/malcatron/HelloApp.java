9a685d8284f7201c9fd9bbc4de1a0a51fa24af59
malcatron <mh55217@yahoo.co.uk>
2016-09-28 16:30:54 +0100

    Added a goodbye message

--- GitTest/src/main/java/com/malcatron/HelloApp.java
+++ GitTest/src/main/java/com/malcatron/HelloApp.java
@@ -9,7 +9,7 @@ public class HelloApp {
      */
     public static void main(String[] args) {
         System.out.println("Hello you Git!");
-
+        System.out.println("Goodbye you Git!");
     }
 
 }
