9df290f09a2b0a7ce4a89e3cc2df3c1ed78a6a71
malcatron <malcatron@malcatron .com>
2016-09-28 16:27:11 +0100

    Project creation

---
+++ GitTest/src/main/java/com/malcatron/HelloApp.java
@@ -0,0 +1,15 @@
+
+
+package com.malcatron;
+
+public class HelloApp {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello you Git!");
+
+    }
+
+}
